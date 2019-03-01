c9703a76d418f1a973cd92d4305c0d382a03410c
brandonconover90@gmail.com <brandonconover90@gmail.com>
2019-02-28 18:47:30 -0700

    setup

---
+++ assets/javascript/game.js
@@ -0,0 +1,9 @@
+// randomly select a winningNumber between 19-120
+// randomly generate values for gems(x4) 1-12
+// total score set to 0
+
+// on click: gem adds number to total score
+// if total = winningNumber wins++ & reset
+// if total > winningNumber losses-- & reset
+// create a reset (like the initializeCalculator) 
+//    reset will create new winningNumber and gem numbers, and set total score 0
